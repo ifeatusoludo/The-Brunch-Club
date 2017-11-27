dee01166d7fe6873dcf79f263328ed23bbc1789b
Connie <itsmeconniec@gmail.com>
2017-11-27 12:25:47 +0000

    Added Google API map successfully + other changes
    Added the Google API Map successfully (no markers) and also aligned the navigation bar to click and lead you further down the page to the corresponding section

--- Brunch club.js
+++ Brunch club.js
@@ -1,11 +1,9 @@
-var map;
-
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {
-			lat: -34.397,
-			lng: 150.644
-		},
-		zoom: 8
-	});
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
+
+$('.carousel').carousel()
